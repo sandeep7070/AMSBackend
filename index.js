b81f060aa9dd0d8b44e14144e426ae1467e5dee6
Yawi <yawimalik786@gmail.com>
2025-02-10 13:07:29 +0530

    Fixing the cors error

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Change this to your frontend URL
+    origin: "http://localhost:5173/", // Change this to your frontend URL
     credentials: true,
   })
 );
